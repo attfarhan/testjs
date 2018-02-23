ad9ab820bcd0013b2bcfcb2b06e42c4dbac8066a
Farhan Attamimi <farhan@sourcegraph.com>
2018-02-23 13:40:54 -0800

    git push

--- test.js
+++
@@ -1,7 +0,0 @@
-import * as diff from "diff";
-import { diffChars, IDiffResult } from "diff";
-
-diffChars("foo", "bar");
-const crypto = require("crypto");
-
-crypto.randomBytes(16).toString("hex");
---
+++ test.ts
@@ -0,0 +1,5 @@
+import * as diff from "diff";
+import { diffChars, IDiffResult } from "diff";
+import typescript;
+
+diffChars("foo", "bar");
